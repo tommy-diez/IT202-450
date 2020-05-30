d1cf0da281aeb669690a281175e78f01e59a058d
tommy-diez <td275@njit.edu>
2020-05-30 00:05:52 -0400

    PDO conn part 3

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -5,10 +5,12 @@ require("config.php");
 try {
     $con_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
     $db = new PDO($con_string, $dbuser, $dbpass);
+    echo "connected";
 }
 catch(exception $e){
     echo $e->getMEssage();
 }
+
 
 
 ?>
