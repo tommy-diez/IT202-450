f66604bf7c3876dfc1e5086c7a753c09fa051f29
tommy-diez <td275@njit.edu>
2020-05-30 14:27:30 -0400

    Added first table'

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -5,10 +5,23 @@ require("config.php");
 try {
     $con_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
     $db = new PDO($con_string, $dbuser, $dbpass);
+
+    //query
+    $query = "CREATE TABLE `Users` (
+                'id' int auto_increment not null,
+                'email' varchar(100) not null unique,
+                'created' timestamp default current_timestamp,
+                'modified' timestamp default current_timestamp on update current_timestamp,
+                PRIMARY KEY ('id')
+                ) CHARACTER SET utf8 COLLATE utf8_general_ci";
+    $stmt = $db->prepare($query);
+    $r = $stmt->execute();
+    echo var_export($r);
+    
     echo "connected";
 }
 catch(exception $e){
-    echo $e->getMEssage();
+    echo $e->getMessage();
 }
 
 
