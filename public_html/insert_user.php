8118150e46292986b8e53509b8801cdc2eee4d33
tommy-diez <td275@njit.edu>
2020-05-30 19:13:38 -0400

    Insert user file added

---
+++ public_html/insert_user.php
@@ -0,0 +1,24 @@
+<?php
+
+//connecting to database
+require("config.php");
+try {
+    $con_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+    $db = new PDO($con_string, $dbuser, $dbpass);
+    $stmt = $db->prepare("INSERT INTO User (email) VALUES(:email)");
+    $r = $stmt->execute(array(":email"=>"test@test.com"));
+    //query
+
+
+    echo var_export($stmt->errorInfo(), true);
+    echo var_export($r, true);
+}
+
+
+catch(exception $e) {
+    echo $e->getMessage();
+}
+
+?>
+
+
