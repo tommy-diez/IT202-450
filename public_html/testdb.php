f87e36925df08a566057e7b590271456fd9f0e19
tommy-diez <td275@njit.edu>
2020-05-30 15:12:16 -0400

    Debug 3

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -7,24 +7,21 @@ try {
     $db = new PDO($con_string, $dbuser, $dbpass);
 
     //query
-    $query = "CREATE TABLE `Users` (
+    $stmt = $db->prepare("CREATE TABLE `Users` (
                 'id' int auto_increment not null,
                 'email' varchar(100) not null unique,
                 'created' timestamp default current_timestamp,
                 PRIMARY KEY ('id')
-                ) CHARACTER SET utf8 COLLATE utf8_general_ci";
-    $stmt = $db->prepare($query);
-    echo var_export($stmt->errorInfo(), true);
+                ) CHARACTER SET utf8 COLLATE utf8_general_ci");
     $r = $stmt->execute();
+    echo var_export($stmt->errorInfo(), true);
     echo var_export($r, true);
-
-    //echo "connected";
-
 }
+
 catch(exception $e) {
     echo $e->getMessage();
-
 }
+
 ?>
 
 
