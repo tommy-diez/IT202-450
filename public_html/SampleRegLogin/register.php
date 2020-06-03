0f92c2c59c71fd4e4f79718f9ab91b46abcbac44
tommy-diez <td275@njit.edu>
2020-06-03 19:03:20 -0400

    debug 2

--- public_html/SampleRegLogin/register.php
+++ public_html/SampleRegLogin/register.php
@@ -1,24 +1,25 @@
 
 <html>
-<form method = "POST">
-    <label for = "email">Email
+<form method="POST">
+    <label for ="email">Email
     <input type="email" id="email" name="email"/>
     </label>
     <label for="password">Password
     <input type="password" id="password" name ="password"/>
     </label>
     <label for="cpassword">Confirm Password
-    <input type=password" id="cpassword" name="cpassword"/>
+    <input type="password" id="cpassword" name="cpassword"/>
     </label>
     <input type="submit" name="register" value="Register"/>
 </form>
 </html>
 
 <?php
-/*echo var_export($_GET);
+echo var_export($_GET);
 echo var_export($_POST, true);
 echo var_export($_REQUEST, true);
-*/
+
+/*
 if (isset($POST["register"])){
     if (isset($_POST["password"]) && isset ($_POST["cpassword"])) {
         $password = $_POST["password"];
@@ -29,7 +30,8 @@ if (isset($POST["register"])){
         else{
             echo "<div>Passwords do not match!</div>";
         }
-        }
+    }
 }
+*/
 ?>
 
