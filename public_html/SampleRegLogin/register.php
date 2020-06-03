f9d576ed19948dcb5653334ca648caa643f42303
tommy-diez <td275@njit.edu>
2020-06-03 18:39:44 -0400

    Registration form password validation

--- public_html/SampleRegLogin/register.php
+++ public_html/SampleRegLogin/register.php
@@ -15,7 +15,22 @@
 </html>
 
 <?php
-echo var_export($_GET);
+/*echo var_export($_GET);
 echo var_export($_POST, true);
 echo var_export($_REQUEST, true);
+*/
+if (isset($POST["register"])){
+    if (isset($_POST['password']) && isset ($_POST['cpassword'])) {
+        $password = $_POST['password'];
+        $cpassword = $_POST['cpassword'];
+        if ($password == $cpassword){
+            echo "<div>Passwords match!</div>";
+        }
+        else{
+            echo "<div>Passwords do not match</div>";
+        }
+        }
+}
+?>
+
 
