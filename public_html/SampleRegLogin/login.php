c565cbb78dc4b8901486bfde41539c4d4a9e4a25
tommy-diez <td275@njit.edu>
2020-06-04 17:38:01 -0400

    SQL debug in login query

--- public_html/SampleRegLogin/login.php
+++ public_html/SampleRegLogin/login.php
@@ -26,7 +26,7 @@ if (isset($_POST["login"])) {
             $con_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
             try{
                 $db = new PDO($con_string, $dbuser, $dbpass);
-                $stmt = $db->prepare("SELECT from Users where email = :email LIMIT 1");
+                $stmt = $db->prepare("SELECT * from Users where email = :email LIMIT 1");
                 $stmt->bindValue(':email', $email);
                 $stmt->execute();
                 $e = $stmt->errorInfo();
