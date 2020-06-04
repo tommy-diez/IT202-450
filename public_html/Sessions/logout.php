59bcfc36e5a898b16fd0884b7ea3590981e02b35
tommy-diez <td275@njit.edu>
2020-06-04 19:25:53 -0400

    Added logout.php

--- public_html/Sessions/logout.php
+++ public_html/Sessions/logout.php
@@ -1,2 +1,16 @@
 <?php
 session_start();
+session_unset();
+session_destroy();
+echo "<div>You have been logged out</div>";
+echo var_export($_SESSION, true);
+//get and delete session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+
+    setcookie(session_name(), '', time() - 42000,
+        $params['path'], $params['domain'],
+        $params['secure'], $params['httponly']);
+}
+
+
