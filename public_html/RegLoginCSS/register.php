c1659ca4e8d70218bce94a8c7a6c6d72809ea0e1
tommy-diez <td275@njit.edu>
2020-06-10 20:30:58 -0400

    form polish

--- public_html/RegLoginCSS/register.php
+++ public_html/RegLoginCSS/register.php
@@ -9,18 +9,23 @@ include('header.php');
             <div class="reg-form">
                 <form method="POST">
                     <label for="first_name">First Name</label>
+                    <br>
                     <input type="text" id="first_name" name="first_name">
                     <br>
                     <label for="last_name">Last Name</label>
+                    <br>
                     <input type="text" id="last_name" name="last_name">
                     <br>
                     <label for ="email">Email</label>
+                    <br>
                     <input type="email" id="email" name="email"/>
                     <br>
                     <label for="password">Password</label>
+                    <br>
                     <input type="password" id="password" name ="password"/>
                     <br>
                     <label for="cpassword">Confirm Password</label>
+                    <br>
                     <input type="password" id="cpassword" name="cpassword"/>
                     <br>
                     <input type="submit" name="register" value="Register"/>
