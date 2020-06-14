2e41ffafb345045d8fcec5b096a527d3a0f6a6b8
tommy-diez <td275@njit.edu>
2020-06-14 18:13:19 -0400

    Fixed MySQL syntax

--- producttable.sql
+++ producttable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Products(
   ProductCode int PRIMARY KEY AUTO_INCREMENT,
   Product varchar(25) PRIMARY KEY,
-  Price money NOT NULL,
+  Price DECIMAL(13,2) NOT NULL,
   Stock int NOT NULL,
   Description varchar(255) int NOT NULL,
 
