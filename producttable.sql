34a87aac6f1f42115477a8d1b99c9e0358d3f9d1
tommy-diez <td275@njit.edu>
2020-06-14 18:01:43 -0400

    Uploaded sql file

---
+++ producttable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Products(
+  ProductCode int PRIMARY KEY AUTO_INCREMENT,
+  Product varchar(25) PRIMARY KEY,
+  Price money NOT NULL,
+  Stock int NOT NULL,
+  Description varchar(255) int NOT NULL,
+
+);
