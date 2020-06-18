8d795a54617948f3ce2a178323f511669c0493ba
tommy-diez <td275@njit.edu>
2020-06-18 11:08:32 -0400

    Intialized HW5 files

--- public_html/SampleCreateEdit/create_table.php
+++ public_html/SampleCreateEdit/create_table.php
@@ -2,13 +2,27 @@
 ?>
 <html>
     <head>
-
     </head>
-
     <body>
         <form>
-            <label for=""></label>
-            <
+            <label for="product_name">Product Name</label>
+            <input type="text" name="product_name" required>
+            <label for="quantity">Quantity</label>
+            <input type="number" name="quantity">
+            <label for="price"></label>
+            <input type="text" name="price" required>
+            <label for="description"></label>
+            <input type="text" name="description">
         </form>
     </body>
 </html>
+
+<?php
+require("common.inc.php");
+$db = getDB();
+//example usage, change/move as needed
+$stmt = $db->prepare("SELECT * FROM Things");
+$stmt->execute();
+?>
+
+
---
+++ public_html/SampleCreateEdit/edit.php
@@ -0,0 +1,8 @@
+<?php
+require("common.inc.php");
+$db = getDB();
+//example usage, change/move as needed
+$stmt = $db->prepare("SELECT * FROM Things");
+$stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+echo var_export($result, true);
