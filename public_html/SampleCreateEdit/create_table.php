d16bb0f8ffd3e4e659afc879015ef60a061fc0b1
tommy-diez <td275@njit.edu>
2020-06-18 11:34:55 -0400

    Completed create product form

--- public_html/SampleCreateEdit/create_table.php
+++ public_html/SampleCreateEdit/create_table.php
@@ -4,7 +4,7 @@
     <head>
     </head>
     <body>
-        <form>
+        <form method="POST">
             <label for="product_name">Product Name</label>
             <input type="text" name="product_name" required>
             <label for="quantity">Quantity</label>
@@ -13,16 +13,31 @@
             <input type="text" name="price" required>
             <label for="description"></label>
             <input type="text" name="description">
+            <input type="submit">
         </form>
     </body>
 </html>
 
 <?php
 require("common.inc.php");
+
+$product_name = $_POST['product_name'];
+$quantity = $_POST['quantity'];
+$price = $_POST['price'];
+$description = $_POST['description'];
+
+
 $db = getDB();
-//example usage, change/move as needed
-$stmt = $db->prepare("SELECT * FROM Things");
+$query = "INSERT INTO `Products`(name, quantity, price, description) 
+            VALUES(product_name, quantity, price, description)";
+$stmt = $db->prepare($query);
+$stmt->bindValue(':product_name', $product_name);
+$stmt->bindValue(':quantity', $quantity);
+$stmt->bindValue(':price', $price);
+$stmt->bindValue(':description', $description);
 $stmt->execute();
+
+
 ?>
 
 
