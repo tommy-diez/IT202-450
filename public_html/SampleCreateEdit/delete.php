6cd17395fc5e01fadf5ea72163d5110973705a11
tommy-diez <td275@njit.edu>
2020-06-21 19:20:05 -0400

    Built delete product functionality

--- public_html/SampleCreateEdit/delete.php
+++ public_html/SampleCreateEdit/delete.php
@@ -1,7 +1,23 @@
 <?php
 require("common.inc.php");
-$db = getDB();
-//example usage, change/move as needed
-$stmt = $db->prepare("SELECT * FROM Things");
-$stmt->execute();
-?>
+
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $db = getDB();
+    $query = "DELETE FROM Products WHERE id=:id";
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(':id', $id);
+    $stmt->execute();
+    $e = $stmt->errorInfo();
+    if ($e[0] != "00000"){
+        var_export($e, true);
+    }
+    else{
+        echo "Product successfully deleted";
+    }
+}
+else {
+    echo "No id in url";
+}
+
