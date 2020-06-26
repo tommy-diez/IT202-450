6e697303c14ff39e8e0c1a4a0e42454b5079b236
tommy-diez <td275@njit.edu>
2020-06-26 02:59:19 -0400

    Added show list functionality

---
+++ public_html/SampleCreateEdit/list.php
@@ -0,0 +1,37 @@
+<?php
+require('common.inc.php');
+$query = file_get_contents(__DIR__, "/queries/SELECT_ALL_TABLES.sql");
+if (isset($query) && !empty($query)){
+    try{
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+    }
+}
+?>
+
+<?php if(isset($results)): ?>
+    <p>Products</p>
+    <ul>
+        <?php foreach ($results as $row): ?>{
+            <li>
+                <?php echo get($row, "id");
+                 echo get($row, "name");
+                 echo get($row, "quantity");
+                 echo get($row, "price");
+                 echo get($row, "description");
+                 echo get($row, "modified");
+                 echo get($row, 'created');
+                 ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+}
+
+<?php else: ?>
+<p>No products available at this time, sadly</p>
+
+<?php endif; ?>
