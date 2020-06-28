20b122dc602a98476cdefd1f1a45f8f22f52a875
tommy-diez <td275@njit.edu>
2020-06-28 14:23:31 -0400

    Added client side validation

--- public_html/SampleCreateEdit/js/validate_form.js
+++ public_html/SampleCreateEdit/js/validate_form.js
@@ -1,16 +1,24 @@
 function validate() {
 
-    errors = [];
     var product_name = document.getElementById("product_name").value;
     var quantity = document.getElementById("quantity").value;
     var price = document.getElementById("price").value;
     var description = document.getElementById("description").value;
+    var alpha = /^[A-Za-z]+$/; //regular expression for alphabet characters
 
     if (product_name == "" || quantity == "" || price == "" || description ==""){
         alert("Leave no fields empty");
         return false;
     }
-    
+    else if (!alpha.test(product_name) || !alpha.test(description)){
+        alert("Only use words and letters for name and description");
+        return false;
+    }
+    else if (alpha.test(quantity) || alpha.test(price)) {
+        alert("Only use numbers for quantity and price");
+        return false;
+    }
+    else return true;
 
 }
 
