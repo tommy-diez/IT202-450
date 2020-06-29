129253d61138789a05e5229ae8d2fee4b70a8060
tommy-diez <td275@njit.edu>
2020-06-28 22:34:29 -0400

    Ajax exploration

---
+++ public_html/SampleCreateEdit/ajax_products.php
@@ -0,0 +1,21 @@
+<?php
+require('common.inc.php');
+$query = file_get_contents(__DIR__ . 'queries/SELECT_ALL_TABLE.sql');
+$result = array("status"=>"200");
+if (isset($query) && !empty($query)){
+    try{
+        $db = getDB();
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $result['status'] = 200; //successful connection
+        $result['results'] = $results;
+    }
+    catch(Exception $e){
+        $result['status'] = 400;
+        $result['error'] = $e->getMessage();
+    }
+}
+echo json_encode($result);
+?>
+
