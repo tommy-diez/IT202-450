6bcda9908ccffe6f91aa777d6b40514e634063a4
tommy-diez <td275@njit.edu>
2020-07-09 19:54:46 -0400

    login functionality fix

--- public_html/SampleCreateEdit/login.php
+++ public_html/SampleCreateEdit/login.php
@@ -33,7 +33,7 @@ if (isset($_POST["login"])) {
     if (isset($_POST["password"]) && isset ($_POST["email"])) {
         $password = $_POST["password"];
         $email = $_POST["email"];
-        $orderID = getOrderID();
+        //$orderID = getOrderID();
         require 'config.php';
         $con_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
