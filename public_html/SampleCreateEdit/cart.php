110499da5c8fc43ee6ae5fbf99004a63a1aaad77
tommy-diez <td275@njit.edu>
2020-07-08 23:01:29 -0400

    Debug SQL

--- public_html/SampleCreateEdit/cart.php
+++ public_html/SampleCreateEdit/cart.php
@@ -6,13 +6,12 @@ if (isset($_POST['add_cart_submit'])){
     $productID= $_POST['add_cart'];
     $quantity = $_POST['cart_quantity'];
     $db = getDB();
-
     $query = "
               UPDATE Products
               SET quantity = quantity - :quantity
               WHERE id = :id;
               
-              INSERT INTO Cart
+              INSERT INTO Cart(orderID, quantity, userID)
               VALUES(:id, :quantity, :userID);";
     $stmt = $db->prepare($query);
     $stmt->bindValue(':id', $productID);
@@ -23,8 +22,10 @@ if (isset($_POST['add_cart_submit'])){
     if ($e[0] != "00000"){
         echo var_export($e, true);
     }
-    header('Location: list.php');
-
+    else {
+        echo "Value successfully inserted";
+        //header('Location: list.php');
+    }
 
 }
 
