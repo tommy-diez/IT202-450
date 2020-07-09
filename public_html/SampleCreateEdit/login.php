b2d285971be1fa4fade63a5890d707252776a9a0
tommy-diez <td275@njit.edu>
2020-07-09 02:50:53 -0400

    attempted fix for login issue

--- public_html/SampleCreateEdit/login.php
+++ public_html/SampleCreateEdit/login.php
@@ -35,6 +35,7 @@ if (isset($_POST["login"])) {
         require 'config.php';
         $con_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
+                $orderID = getOrderID();
                 $db = new PDO($con_string, $dbuser, $dbpass);
                 $stmt = $db->prepare("SELECT * from Users where email = :email LIMIT 1");
                 $stmt->bindValue(':email', $email);
@@ -50,7 +51,7 @@ if (isset($_POST["login"])) {
                         if(password_verify($password, $rpassword)){
                             echo "<div>Login credentials valid. You are logged in";
                             $_SESSION['user'] = array(
-                                    //"orderID"=>getOrderID(),
+                                    "orderID"=>$orderID,
                                     "id"=>$result['id'],
                                     "email"=>$result["email"],
                                     "first_name"=>$result["first_name"],
