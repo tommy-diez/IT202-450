056ab585f1dd58c60bcf16026c0899a749fbaf60
tommy-diez <td275@njit.edu>
2020-07-09 19:51:58 -0400

    testing login issue

--- public_html/SampleCreateEdit/login.php
+++ public_html/SampleCreateEdit/login.php
@@ -57,7 +57,7 @@ if (isset($_POST["login"])) {
                                     "email"=>$result["email"],
                                     "first_name"=>$result["first_name"],
                                     "last_name"=>$result["last_name"],
-                                    "orderID"=>$orderID,
+                                    //"orderID"=>$orderID,
 
                             );
                             echo var_export($_SESSION, true);
