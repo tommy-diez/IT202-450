398b5ef8bf69c08a5c568dae11792fb17f1a95b2
tommy-diez <td275@njit.edu>
2020-07-11 00:58:45 -0400

    attached method

--- public_html/SampleCreateEdit/edit_cart.php
+++ public_html/SampleCreateEdit/edit_cart.php
@@ -9,26 +9,8 @@ if(isset($_POST['submit'])) {
     echo $productID;
     echo $quantity;
     //var_export($db, true);
-    $query = "
-UPDATE
-   Cart
-SET
-   productID = :newProductID, quantity = :quantity
-WHERE
-   orderID = :orderID";
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(':newProductID', $productID);
-    $stmt->bindValue(':quantity', $quantity);
-    $stmt->bindValue(':orderID', $id);
-    $e = $stmt->errorInfo();
-    if ($e[0] != "00000"){
-        echo var_export($e, true);
-    }
-    else{
-        echo "Successfully updated cart";
-        header('Location: index.php');
-    }
-    //header('Location: index.php');
+    modifyCart($id, $quantity, $productID);
+    header('Location: index.php');
 }
 else {
     echo "<h1>Invalid Request</h1>";
