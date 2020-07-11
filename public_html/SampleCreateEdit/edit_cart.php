496d1129eff5666165f810b63ff97871f1e7532c
tommy-diez <td275@njit.edu>
2020-07-10 23:54:48 -0400

    One more time

--- public_html/SampleCreateEdit/edit_cart.php
+++ public_html/SampleCreateEdit/edit_cart.php
@@ -1,17 +1,15 @@
 <?php
-//include('header.php');
+include('header.php');
+include 'common.inc.php';
 if(isset($_POST['submit'])) {
     $id = $_POST['id'];
     $productID = $_POST['product'];
     $quantity = $_POST['quantity'];
-    //$db = getDB();
+    $db = getDB();
     echo $id;
     echo $productID;
     echo $quantity;
     //var_export($db, true);
-}
-
-    /*
     $query = "
 UPDATE
    Cart
@@ -37,4 +35,4 @@ WHERE
 else {
     echo "<h1>Invalid Request</h1>";
 }
-*/
+
