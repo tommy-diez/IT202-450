97d6498eab04eebed167989f397753e82e307cf5
tommy-diez <td275@njit.edu>
2020-07-10 22:29:42 -0400

    setup echo var

--- public_html/SampleCreateEdit/edit_cart.php
+++ public_html/SampleCreateEdit/edit_cart.php
@@ -3,11 +3,17 @@ if(isset($_POST['submit'])) {
     $id = $_POST['id'];
     $productID = $_POST['product'];
     $quantity = $_POST['quantity'];
+    /*
     echo $id;
     echo $productID;
     echo $quantity;
+    */
     modifyCart($id, $quantity, $productID);
+    $e = $stmt->errorInfo();
+    if ($e[0] != "00000"){
+        echo var_export($e, true);
+    }
     header('Location:index.php');
 }
-?>
+
 
