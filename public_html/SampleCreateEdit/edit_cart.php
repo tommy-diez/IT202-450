3c9635c34510f3293a53cce8b7172a4b2e21f60a
tommy-diez <td275@njit.edu>
2020-07-10 22:40:44 -0400

    Fix

--- public_html/SampleCreateEdit/edit_cart.php
+++ public_html/SampleCreateEdit/edit_cart.php
@@ -3,11 +3,6 @@ if(isset($_POST['submit'])) {
     $id = $_POST['id'];
     $productID = $_POST['product'];
     $quantity = $_POST['quantity'];
-    /*
-    echo $id;
-    echo $productID;
-    echo $quantity;
-    */
     $db = getDB();
     $query = "
 UPDATE
@@ -20,6 +15,7 @@ WHERE
     $stmt->bindValue(':newProductID', $productID);
     $stmt->bindValue(':quantity', $quantity);
     $stmt->bindValue(':orderID', $id);
+    $e = $stmt->errorInfo();
     if ($e[0] != "00000"){
         echo var_export($e, true);
     }
