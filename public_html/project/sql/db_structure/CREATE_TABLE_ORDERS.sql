fad8f6d49736df966da697d404cf77f03d4a05e6
tommy-diez <td275@njit.edu>
2020-07-21 13:32:12 -0400

    Added final table to db structure

---
+++ public_html/project/sql/db_structure/CREATE_TABLE_ORDERS.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `Orders` (
+    `id` int NOT NULL auto_increment,
+    `order_id` int NOT NULL UNIQUE,
+    `quantity` int NOT NULL,
+    `price` decimal(12,2),
+    `user_id` int,
+    `paid_total` decimal (12,2),
+    `created` timestamp default current_timestamp,
+    `modified` timestamp default current_timestamp on update current_timestamp,
+    PRIMARY KEY(`id`),
+    FOREIGN KEY (`user_id`)
+)
