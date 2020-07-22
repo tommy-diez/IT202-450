d5e31a562a60cfcafea418948ee386b96080e683
tommy-diez <td275@njit.edu>
2020-07-22 08:32:58 -0400

    Testing out reg

--- public_html/project/register.php
+++ public_html/project/register.php
@@ -1,7 +1,6 @@
 <?php
 include_once(__DIR__ . "/partial-pages/header.php");
 ?>
-
 <div>
     <form method="POST">
         <label for="first_name">First Name: </label>
@@ -16,11 +15,12 @@ include_once(__DIR__ . "/partial-pages/header.php");
         <br>
         <label for="cpassword">Confirm Password: </label>
         <input type="password" id="cpassword" name="cpassword" required min="5">
-        <input type="submit" name="submit" value="SUBMIT">
+        <input type="submit" name="register" value="SUBMIT">
     </form>
 </div>
-
 <?php
+
+/*
 if (Common::get($_POST, "submit", false)){
     $email = Common::get($_POST, "email", false);
     $password = Common::get($_POST, "password", false);
@@ -47,3 +47,58 @@ if (Common::get($_POST, "submit", false)){
         die(header("Location: register.php"));
     }
 }
+*/
+
+if (isset($_POST["register"])) {
+    if (isset($_POST["password"]) && isset ($_POST["cpassword"])) {
+        if (isset($_POST["email"])) {
+            $first_name = $_POST["first_name"];
+            $last_name = $_POST["last_name"];
+            $password = $_POST["password"];
+            $cpassword = $_POST["cpassword"];
+            $email = $_POST["email"];
+            //echo $password;
+            //echo $cpassword;
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                if ($password == $cpassword) {
+                    //echo "<br>Passwords match!";
+                    require 'config.php';
+                    $con_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+                    try{
+                        $db = new PDO($con_string, $dbuser, $dbpass);
+                        $hash = password_hash($password, PASSWORD_BCRYPT);
+                        $stmt = $db->prepare("INSERT INTO Users (first_name, last_name, email, password) 
+                                                VALUES (:first_name, :last_name, :email, :password)");
+                        $stmt->bindValue(':email', $email);
+                        $stmt->bindValue(':password', $hash);
+                        $stmt->bindValue(':first_name', $first_name);
+                        $stmt->bindValue(':last_name', $last_name);
+                        $stmt->execute();
+                        $e = $stmt->errorInfo();
+                        if ($e[0] != "00000") {
+                            echo var_export($e, true);
+                        } else {
+                            echo "<div>Account successfully created!";
+                        }
+                    } catch (Exception $e) {
+                        echo $e->getMessage();
+                    }
+                } else {
+                    echo "<br>Passwords do not match!";
+                }
+
+            }
+            else {
+                echo "<div>Must use valid email address</div>";
+            }
+
+        }
+        else {
+            echo "<div>Leave no fields empty</div>";
+        }
+    }
+    else{
+        echo "<div>Leave no fields empty</div>";
+    }
+}
+?>
