c0004dd2af12419d9e08ffcb4d6dd6ee30b27677
tommy-diez <td275@njit.edu>
2020-07-23 14:04:34 -0400

    fix

--- public_html/project/tester.php
+++ public_html/project/tester.php
@@ -3,7 +3,7 @@ include 'includes/config.php';
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabse;charset=utf8mb4";
 //$this->db =
 $db = new PDO($connection_string, $dbuser, $dbpass);
-    $stmt = $db->prepare("SELECT * FROM Users");
+    $stmt = $db->prepare("SELECT * FROM Users where id=8");
     $stmt->execute();
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     var_dump($results);
