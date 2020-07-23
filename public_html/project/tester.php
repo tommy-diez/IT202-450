01aa69e46496d623b90e09e36d1e4eef2cbe6f31
tommy-diez <td275@njit.edu>
2020-07-23 13:50:18 -0400

    Debug

--- public_html/project/tester.php
+++ public_html/project/tester.php
@@ -2,8 +2,7 @@
 include 'includes/config.php';
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabse;charset=utf8mb4";
 //$this->db =
-    $db = new PDO($connection_string, $dbuser, $dbpass);
-    $db = $common->getDB();
+$db = new PDO($connection_string, $dbuser, $dbpass);
     $stmt = $db->prepare("SELECT * FROM Users");
     $stmt->execute();
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
