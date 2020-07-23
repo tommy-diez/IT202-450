f3ba6ecee80103d7aa39cdae88a3cdc776f48430
tommy-diez <td275@njit.edu>
2020-07-23 15:51:40 -0400

    Fixed connectivity

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -31,10 +31,9 @@ if (isset($_POST["login"])) {
     if (isset($_POST["password"]) && isset ($_POST["email"])) {
         $password = $_POST["password"];
         $email = $_POST["email"];
-        require 'config.php';
+        require 'includes/config.php';
         $con_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
-
             $db = new PDO($con_string, $dbuser, $dbpass);
             $stmt = $db->prepare("SELECT * from Users where email = :email LIMIT 1");
             $stmt->bindValue(':email', $email);
