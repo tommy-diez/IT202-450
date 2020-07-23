a46a6385bd946da695078885c2de220c24c005aa
tommy-diez <td275@njit.edu>
2020-07-23 13:32:24 -0400

    Tester debug

--- public_html/project/tester.php
+++ public_html/project/tester.php
@@ -1,12 +1,8 @@
 <?php
 include 'includes/common.inc.php';
-try {
+
     $db = $common->getDB();
     $stmt = $db->prepare("SELECT * FROM Users");
     $stmt->execute();
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     var_export($results, true);
-}
-catch (Exception $e) {
-    echo $e;
-}
