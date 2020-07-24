654245f7fa035fa88b0bf18e0a69a8b8662b1ee5
tommy-diez <td275@njit.edu>
2020-07-24 09:27:41 -0400

    turn on error reporting

--- public_html/project/tester.php
+++ public_html/project/tester.php
@@ -1,5 +1,9 @@
 <?php
 include_once (__DIR__ . 'partial-pages/header.php');
+error_reporting(-1); // reports all errors
+ini_set("display_errors", "1"); // shows all errors
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
 
 $arr = array(
     "name"=>"John",
