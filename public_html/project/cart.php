ffab3b02e2a0e12d46f28fc278c5483969ef39ad
tommy-diez <td275@njit.edu>
2020-07-25 14:57:20 -0400

    polishing form

--- public_html/project/cart.php
+++ public_html/project/cart.php
@@ -23,16 +23,6 @@ var_dump($cart);
             <?php echo Common::get($item, 'quantity'); ?>
         </td>
         <td>
-            <form method="POST" action="edit_cart.php">
-                <input id="id" type="hidden" name="id" value="<?php echo get($item, 'orderID'); ?>">
-                <label for="product">Product ID</label>
-                <input id="product" type="number" name="product">
-                <br>
-                <label for="quantity">Quantity</label>
-                <input id="quantity" type="number"  name="quantity">
-                <br>
-                <input type="submit" name="submit" value="EDIT CART">
-            </form>
         </td>
     </tr>
     <?php endforeach; ?>
