9da224ab1c7f6b0bae82b928fef02766557d23fb
tommy-diez <td275@njit.edu>
2020-07-25 13:21:56 -0400

    testing multi-dem arrays

--- public_html/project/tester.php
+++ public_html/project/tester.php
@@ -5,12 +5,38 @@ ini_set("display_errors", "1"); // shows all errors
 ini_set("log_errors", 1);
 ini_set("error_log", "/tmp/php-error.log");
 
+$multi_arr = array(
 $arr = array(
     "name"=>"John",
-    "age"=>"34",
+    "age"=>34,
     "profession"=>"carpenter",
     "country"=>"USA"
+),
+$arr2 = array(
+    "name"=>"Mark",
+    "age"=>15,
+    "profession"=>"student",
+    "country"=>"USA"
+)
 );
-echo(Common::get($arr, "name"));
-
 ?>
+
+<?php foreach ($multi_arr as $arr): ?>
+    <tr>
+        <td>
+            <?php echo Common::get($arr, 'name'); ?>
+        </td>
+        <td>
+            <?php echo Common::get($arr, 'age'); ?>
+        </td>
+        <td>
+            <?php echo Common::get($arr, 'profession'); ?>
+        </td>
+        <td>
+            <?php echo Common::get($arr, 'country'); ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+
+
+
