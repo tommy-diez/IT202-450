8c219ed635d17ce5701e6c1c2f2b1d0c064a0248
tommy-diez <td275@njit.edu>
2020-07-26 19:34:49 -0400

    sql debug

--- public_html/project/sql/queries/place_order.sql
+++ public_html/project/sql/queries/place_order.sql
@@ -2,6 +2,6 @@ INSERT INTO Orders (OrderID, productID, quantity, userID, paidTotal)
 VALUES (:OrderID, :productID, :quantity, :userID, :paidTotal);
 
 UPDATE Products
-SET quantity = quantity - 1
+SET quantity = quantity - :quantity
 WHERE id = :productID
 
