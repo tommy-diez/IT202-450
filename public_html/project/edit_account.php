f38c94a3437b39f4dcd62c2e0bc93520b5cc6a05
tommy-diez <td275@njit.edu>
2020-08-02 22:31:50 -0400

    fixing reset pass v3

--- public_html/project/edit_account.php
+++ public_html/project/edit_account.php
@@ -3,13 +3,11 @@ include_once(__DIR__ . "/partial-pages/header.php");
 if(Common::is_logged_in()){
 
 }
-if(isset($_GET['id'])) {
+if(isset($_GET['id'])):  {
     $id = $_GET['id'];
-    $user = DBH::getUserInfo($id);
+
 }
 ?>
-<? if(!empty($user)) : ?>
-<form>
     <form method="POST">
         <label for="password">Password: </label>
         <input id="password" type="password" name="password">
@@ -17,16 +15,23 @@ if(isset($_GET['id'])) {
         <input id="cpassword" type="password" name="cpassword">
         <input type="submit" name="pass_reset" value="RESET">
     </form>
-</form>
-
 <?php
 if(!empty($_POST['pass_reset'])){
     $pass = $_POST['password'];
     $cpass = $_POST['cpassword'];
-    $result = DBH::resetPass($pass, $cpass, $id);
+    if($pass == $cpass) {
+        $result = DBH::resetPass($pass, $cpass, $id);
+    }
+    else{
+        echo"<p>New passwords must match</p>";
+    }
     if($result){
         Common::flash('Successfully updated password');
         header('Location: account.php');
+    }
+    else{
+          Common::flash('Failed to reset password');
+          header('Location: account.php'); ?>
     }
 }
 
